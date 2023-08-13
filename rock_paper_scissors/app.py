e811d3f7b4e1b59276e16ab0a4570aa78fed1829
blister-pack <andrecrujo@hotmail.com>
2023-08-13 21:19:29 +0200

    game is fully functional except it doesn't end

--- rock_paper_scissors/app.py
+++ rock_paper_scissors/app.py
@@ -20,36 +20,60 @@ class RockPaperScissorsApp(QtWidgets.QMainWindow, Ui_RockPaperScissors):
 
         self.selected_move = "Rock"
 
+        self.player_score = 0
+        self.pc_score = 0
+
+        self.rock_img = QtGui.QPixmap(
+            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/rock.png"
+        )
+        self.paper_img = QtGui.QPixmap(
+            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/paper.png"
+        )
+        self.scissors_img = QtGui.QPixmap(
+            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/scissors.png"
+        )
+
     def player_image_move_change(self):
         """
         this function changes the image that represents
         the player's choice for a move
         it's supposed to change when the player changes the move selected
         """
-        rock_img = QtGui.QPixmap(
-            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/rock.png"
-        )
-        paper_img = QtGui.QPixmap(
-            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/paper.png"
-        )
-        scissors_img = QtGui.QPixmap(
-            "/home/blister-pack/Documents/code_projects/1WORK/Projects/rock_paper_scissors/sprites/scissors.png"
-        )
 
         self.selected_move = self.comboBox.currentText()
 
         if self.selected_move == "Rock":
-            self.player_move_label.setPixmap(rock_img)
+            self.player_move_label.setPixmap(self.rock_img)
         elif self.selected_move == "Paper":
-            self.player_move_label.setPixmap(paper_img)
+            self.player_move_label.setPixmap(self.paper_img)
         elif self.selected_move == "Scissors":
-            self.player_move_label.setPixmap(scissors_img)
+            self.player_move_label.setPixmap(self.scissors_img)
 
     def pc_move_image_change(self):
-        pass
+        if self.pc_move.data == "rock":
+            self.pc_move_label.setPixmap(self.rock_img)
+        elif self.pc_move.data == "paper":
+            self.pc_move_label.setPixmap(self.paper_img)
+        elif self.pc_move.data == "scissor":
+            self.pc_move_label.setPixmap(self.scissors_img)
 
-    def change_scores(self):
-        pass
+    def change_scores(self, round_result):
+        if round_result == True:
+            self.player_score += 1
+        elif round_result == False:
+            self.pc_score += 1
+
+        self.player_score_label.setText(str(self.player_score))
+        self.pc_score_label.setText(str(self.pc_score))
+
+    def winner_check(self):
+        """right now this func is just a representation, ideally it will
+        make a popup window stating the winner and exit the program"""
+
+        if self.player_score == 3:
+            print("Player wins")
+        if self.pc_score == 3:
+            print("PC wins")
 
     # this game needs to be played differently than the original logic
     # originally, the logic was supposed to keep the game going until
@@ -62,13 +86,12 @@ class RockPaperScissorsApp(QtWidgets.QMainWindow, Ui_RockPaperScissors):
             the strings inside the logic, only then can I get the
             right move
             """
-
-        if self.selected_move == "Rock":
-            self.selected_move = "rock"
-        elif self.selected_move == "Paper":
-            self.selected_move = "paper"
-        elif self.selected_move == "Scissors":
-            self.selected_move = "scissor"
+            if self.selected_move == "Rock":
+                self.selected_move = "rock"
+            elif self.selected_move == "Paper":
+                self.selected_move = "paper"
+            elif self.selected_move == "Scissors":
+                self.selected_move = "scissor"
 
         # hope this doesn't cause conflicts with the image changing function
         # I still get the object associated with seleted_move and not
@@ -76,10 +99,16 @@ class RockPaperScissorsApp(QtWidgets.QMainWindow, Ui_RockPaperScissors):
 
         reformat_selected_move()
         chosen_move = player_move(self.selected_move)
+        self.pc_move = computer_move()
+        round_result = check_round_win(chosen_move, self.pc_move)
+
         print(chosen_move.data)
-        pc_move = computer_move()
-        print(pc_move.data)
-        # I think I need to use the play_game function and am doing this wrong
+        print(self.pc_move.data)
+        print(round_result)
+
+        self.pc_move_image_change()
+        self.change_scores(round_result)
+        self.winner_check()
 
 
 if __name__ == "__main__":
