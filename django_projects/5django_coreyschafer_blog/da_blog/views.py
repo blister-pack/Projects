e986ef48117975ccc2a96064d0820b5603454113
blister-pack <andrecrujo@hotmail.com>
2023-10-31 10:06:59 +0100

    idk

--- django_projects/5django_coreyschafer_blog/da_blog/views.py
+++ django_projects/5django_coreyschafer_blog/da_blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        "author": "DJ_BlyatMan",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "October 26, 2023",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Jane's hair routine",
-        "content": "You should be drying your hair by vigorously whipping it back and forth",
-        "date_posted": "October 22, 2023",
-    },
-]
+# posts = [
+#     {
+#         "author": "DJ_BlyatMan",
+#         "title": "Blog Post 1",
+#         "content": "First post content",
+#         "date_posted": "October 26, 2023",
+#     },
+#     {
+#         "author": "Jane Doe",
+#         "title": "Jane's hair routine",
+#         "content": "You should be drying your hair by vigorously whipping it back and forth",
+#         "date_posted": "October 22, 2023",
+#     },
+# ]
 
 
 def home(request):
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
     return render(request, "da_blog/home.html", context)
 
