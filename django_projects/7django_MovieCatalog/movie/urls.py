885579cfa1c9e240f88b2932627172d7b38afe3e
blister-pack <andrecrujo@hotmail.com>
2023-11-12 23:57:13 +0100

    finished impementing searches

--- django_projects/7django_MovieCatalog/movie/urls.py
+++ django_projects/7django_MovieCatalog/movie/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.movie_list, name="movie-list"),
     path("<int:pk>/", views.movie_details, name="movie-details"),
+    path("search/", views.search_bar, name="search-bar"),
     path("search/title=<str:term>/", views.search_movies, name="movie-search"),
+    path("good_search/", views.good_search_bar, name="gud-search"),
     path("create_movie/", views.create_movie, name="create-movie"),
 ]
 
--- django_projects/7django_MovieCatalog/movie/views.py
+++ django_projects/7django_MovieCatalog/movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from movie.forms import CreateMovieForm
+
 from .models import Movie
 from django.contrib import messages
 
@@ -22,6 +23,24 @@ def movie_details(request, pk):
 def search_movies(request, term):
     movies = Movie.objects.filter(title__icontains=term)
     return render(request, "movie/movie_search.html", {"movies": movies})
+
+
+def search_bar(request):
+    if request.method == "POST":
+        search_term = request.POST.get("search_term")
+        return redirect("movie-search", term=search_term)
+    return render(request, "movie/search_bar.html")
+
+
+def good_search_bar(request):
+    g_search_term = request.GET.get("good_search_term")
+
+    if g_search_term:
+        movies = Movie.objects.filter(title__icontains=g_search_term)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, "movie/good_search.html", {"movies": movies})
 
 
 def create_movie(request):
